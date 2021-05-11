eb8fe6f46031b77e652de270e1907836243d8a42
Twinkal <twinkalpatel1710@outlook.com>
2021-05-11 12:29:48 -0700

    GUI2

---
+++ GUI.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import Label,messagebox,Button,Entry,TOP,BOTTOM
+import webbrowser
+
+root = tk.Tk()
+root.geometry("300x100")
+root.minsize(300,100)
+root.maxsize(300,100)
+
+root.title("Multiple open URL")
+
+
+
+labal1 = Label(text = "Enter URL:")
+labal1.pack()
+
+
+E1 = Entry()
+E1.pack()
+
+def url():
+    webbrowser.Chrome("C:/Program Files/Google/Chrome/Application/chrome.exe").open_new(E1.get())    
+
+
+button1 = Button(text = "Open URL", command = url, fg = "green")
+button1.pack()
+
+root.mainloop()
