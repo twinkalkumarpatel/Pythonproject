a90d184397bc074d6dfdbd8130c108451bcc30e3
twinkalkumarpatel <80961823+twinkalkumarpatel@users.noreply.github.com>
2021-05-16 06:17:56 -0700

    Update Birthdaywish.py

--- Birthdaywish.py
+++ Birthdaywish.py
@@ -7,6 +7,6 @@ df = pd.read_excel("Birthday.xlsx")
 today = datetime.datetime.now().strftime("%d-%m")
 
 for index, item in df.iterrows():
-    bday = item['Date of Birth'].strftime("%d-%m")
+    bday = item['Date of Births'].strftime("%d-%m")
     if (today == bday):
         print(item['Date of Birth'],item['Name'])
